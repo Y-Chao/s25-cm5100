67a10890c9a94dfc78e9ffc9059934e349dac424
Chao Yang <yang_sustain@126.com>
2025-09-08 21:52:51 +0800

    modify config.sh

--- config_hpc.sh
+++ config_hpc.sh
@@ -19,23 +19,61 @@ check_hpc() {
 
 # --- Step 1: Create conda environment ---
 echo "[INFO] Creating conda environment: cm5100"
-server=check_hpc()
-if [ server == "NSCC" ];then
+server=$(check_hpc)
+if [ $server == "NSCC" ];then
     module load miniforge3
-    conda create -n "cm5100" python=3.12 -y
-    pip install pymatgen
-    pip install mace
-elif [ server == "VANDA" ];then
+    # For the first time use conda in a new shell
+    eval "$(conda shell.bash hook)"
+    condaenv=$(conda env list | grep cm5100)
+    if [ ! -n "$condaenv" ]; then
+        echo "Conda environment cm5100 does not exist. Creating..."
+        conda create -n "cm5100" python=3.12 -y
+        conda activate cm5100
+        pip install pymatgen
+        pip install mace
+    else
+        echo "Conda environment cm5100 already exists. Activating..."
+        conda activate cm5100
+    fi
+elif [ $server == "VANDA" ];then
     module load Miniconda3/24.7.1-0
-    conda create -n "cm5100" python=3.12 -y
-    pip install pymatgen
-    pip install mace
+    # For the first time use conda in a new shell
+    eval "$(conda shell.bash hook)"
+    condaenv=$(conda env list | grep cm5100)
+    if [ ! -n "$condaenv" ]; then
+        echo "Conda environment cm5100 does not exist. Creating..."
+        conda create -n "cm5100" python=3.12 -y
+        conda activate cm5100
+        pip install pymatgen
+        pip install mace
+    else
+        echo "Conda environment cm5100 already exists. Activating..."
+        conda activate cm5100
+    fi
 else
     echo "Wrong server"
     exit 0
+fi
 
-# --- Step 2: Download prerequisite files ---
-echo "[INFO] Download needed files to current directory"
+# --- Step 2: Modify the ase calculator  ---
+echo "[INFO] Patching the ase calculator"
+ase_path=$(python -c "import ase; print(ase.__path__[0])")
+echo "Finding ase path: $ase_path"
+sed -i "/'ibrion',/a\ \ \ \ 'isearch'," $ase_path/calculators/vasp/create_input.py
 
 # --- Step 3: Set environment variables ---
 echo "[INFO] Setting environment variables"
+read -p "Please input IP address: " ip
+if [ -z "$ip" ]; then
+    echo "No IP address provided. Exiting."
+    exit 1
+fi
+cd data/dft/
+echo "Downloading vasp and potential files from ${ip}"
+wget "${ip}/vasp_651_intel_mkl_std"
+wget "${ip}/potpaw_PBE.64.tar"
+
+mkdir potpaw_PBE
+tar -xvf potpaw_PBE.64.tar -C ${PWD}/potpaw_PBE
+
+echo "Finished"
